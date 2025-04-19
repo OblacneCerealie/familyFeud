8353a4d070a5cf044086cba0a7e150a1925732d6
Sebastian <bastymichalko@gmail.com>
2025-04-19 20:46:55 +0200

    pretty much finished html and css, time to shine Samo

---
+++ script.js
@@ -0,0 +1,23 @@
+const answers = [
+    "Survey says: Pizza!",
+    "Survey says: Vacation!",
+    "Survey says: Ice Cream!",
+    "Survey says: Family Time!",
+    "Survey says: Road Trip!",
+    "Survey says: Beach!",
+    "Survey says: Movies!",
+    "Survey says: Laughter!"
+  ];
+  
+  document.querySelectorAll('.flip-card').forEach((card, index) => {
+    const button = card.querySelector('.front');
+    const back = card.querySelector('.back');
+  
+    button.addEventListener('click', () => {
+      if (!card.classList.contains('flipped')) {
+        const randomAnswer = answers[index] || "Survey says: ???";
+        back.textContent = randomAnswer;
+        card.classList.add('flipped');
+      }
+    });
+  });
