7c72b78439524970d300d30ffa42a93b58535bae
samuel907 <samuel.sidivar@gmail.com>
2025-04-20 19:27:43 +0200

    Setupovanie scoring systemu, zfunkcnovanie mien timov, trackovanie skore timov pomocou localStorage

--- script.js
+++ script.js
@@ -21,6 +21,37 @@ document.querySelectorAll(".flip-card").forEach((card, index) => {
     }
   });
 });
-document.getElementById("ready").addEventListener("click", () => {
-  document.getElementById("teamOne");
+
+// Scoring System
+const scoring = new Map();
+scoring
+  .set(1, 800)
+  .set(2, 700)
+  .set(3, 600)
+  .set(4, 500)
+  .set(5, 400)
+  .set(6, 300)
+  .set(7, 200)
+  .set(8, 100);
+let currTeam = 1;
+let team1Score = parseInt(localStorage.getItem("teamOneScore"));
+let team2Score = parseInt(localStorage.getItem("teamTwoScore"));
+
+// Selectovanie elementov na otazkach a pripocitavanie bodov timom za otazky
+for (let x = 1; x < 9; x++) {
+  const odpovedBtn = document.getElementById(String(x));
+  if (odpovedBtn) {
+    odpovedBtn.addEventListener("click", function () {
+      console.log("CLICK");
+      if (currTeam === 1) {
+        team1Score += scoring.get(x);
+      } else {
+        team2Score += scoring.get(x);
+      }
+    });
+  }
+}
+document.getElementById("back").addEventListener("click", () => {
+  localStorage.setItem("teamOneScore", team1Score);
+  localStorage.setItem("teamTwoScore", team2Score);
 });
---
+++ scriptIndex.js
@@ -0,0 +1,3 @@
+// JS file pre prepinanie medzi oknami -- iba na displayovanie skore -- mozno neskor spravim iba ako inline kod
+let score1 = localStorage.getItem("teamOneScore");
+let score2 = localStorage.getItem("teamTwoScore");
